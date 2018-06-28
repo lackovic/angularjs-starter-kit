535227f93e02d4cef5be71694bad7b784b3e6e7a
Marco Lackovic <marco.lackovic@gmail.com>
2018-06-28 23:00:21 +0300

    Fix empty home page on refresh

--- src/app.config.js
+++ src/app.config.js
@@ -7,7 +7,7 @@
     appModule.value('debuggingBorder', true);
     appModule.value('apiBase', 'https://jsonplaceholder.typicode.com/');
 
-    appModule.config(function ($stateProvider) {
+    appModule.config(function ($stateProvider, $urlRouterProvider) {
         var states = [{
             name: 'home',
             url: '',
@@ -28,6 +28,9 @@
         states.forEach(function (state) {
             $stateProvider.state(state);
         });
+        $urlRouterProvider.when('/', ['$state', '$match', function ($state, $match) {
+            $state.go('home');
+        }]);
     });
 
 })();
