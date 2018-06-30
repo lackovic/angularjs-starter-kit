ae11fa3415629f8b57c9c741785b33168bfc6a4f
Marco Lackovic <marco.lackovic@gmail.com>
2018-06-30 11:08:15 +0300

    Change page title on page change

--- src/app.config.js
+++ src/app.config.js
@@ -2,31 +2,53 @@
 (function () {
     'use strict';
 
-    const appModule = angular.module('app');
+    angular.module('app')
 
-    appModule.value('debuggingBorder', true);
-    appModule.value('apiBase', 'https://jsonplaceholder.typicode.com/');
+        .run(
+            ['$rootScope', '$state', '$stateParams',
+                function ($rootScope, $state, $stateParams) {
+                    // It's very handy to add references to $state and $stateParams to the $rootScope
+                    // so that you can access them from any scope within your applications.For example,
+                    // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+                    // to active whenever 'contacts.list' or one of its decendents is active.
+                    $rootScope.$state = $state;
+                    $rootScope.$stateParams = $stateParams;
+                }
+            ]
+        )
 
-    appModule.config(function ($stateProvider, $urlRouterProvider) {
-        var states = [{
-            name: 'home',
-            url: '',
-            template: '<home></home>'
-        }, {
-            name: 'albums',
-            url: '/albums',
-            template: '<albums></albums>'
-        }, {
-            name: 'posts',
-            url: '/posts',
-            template: '<posts></posts>'
-        }];
-        states.forEach(function (state) {
-            $stateProvider.state(state);
+        .value('debuggingBorder', true)
+        .value('apiBase', 'https://jsonplaceholder.typicode.com/')
+
+        .config(function ($stateProvider, $urlRouterProvider) {
+            var states = [{
+                name: 'home',
+                url: '',
+                template: '<home></home>',
+                data: {
+                    pageTitle: 'Home'
+                }
+            }, {
+                name: 'albums',
+                url: '/albums',
+                template: '<albums></albums>',
+                data: {
+                    pageTitle: 'Albums'
+                }
+            }, {
+                name: 'posts',
+                url: '/posts',
+                template: '<posts></posts>',
+                data: {
+                    pageTitle: 'Posts'
+                }
+            }];
+            states.forEach(function (state) {
+                $stateProvider.state(state);
+            });
+            $urlRouterProvider.when('/', ['$state', '$match', function ($state, $match) {
+                $state.go('home');
+            }]);
         });
-        $urlRouterProvider.when('/', ['$state', '$match', function ($state, $match) {
-            $state.go('home');
-        }]);
-    });
 
 })();
